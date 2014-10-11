59c085f09b0513032d2b798bab920d714203a6e1
Fyxerz <fyxerz@hotmail.com>
2014-10-11 03:17:30 +0100

    HTML, CSS, JavaScript and image assets.

---
+++ Pedz stuffz/header.php
@@ -0,0 +1,13 @@
+
+<head>
+	<title></title>
+	<link rel="stylesheet" href="./css/reset.css">
+	<link rel="stylesheet" href="./css/style.css">
+	<link rel="stylesheet" href="./css/720_grid.css" type="text/css" media="screen and (min-width: 720px)">
+	<link rel="stylesheet" href="./css/986_grid.css" type="text/css" media="screen and (min-width: 986px)">
+	<link rel="stylesheet" href="./css/1236_grid.css" media="screen and (min-width: 1236px)" >
+	<link rel="stylesheet" href="./css/animate.css">
+	<link rel="stylesheet" href="./css/liquid-slider.css">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,100' rel='stylesheet' type='text/css'>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
---
+++ Pedz stuffz/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<?php include('header.php') ?>
+<body>
+
+<div class="slider_wrap">
+  <div class="slider">
+    <div class="item">
+		<div class="wrapper">
+				<div class="category">
+					<span class="prev_post">
+						November 2014
+					</span>
+					<span class="next_post">
+						September 2014
+					</span>
+					<h2>
+						October 2014
+					</h2>
+				</div>
+			</div>
+	    </div>
+	    <div class="item">
+		<div class="wrapper wrapper2">
+				<div class="category">
+					<span class="prev_post">
+						October 2014
+					</span>
+					<span class="next_post">
+						July 2014
+					</span>
+					<h2>
+						September 2014
+					</h2>
+				</div>
+			</div>
+	    </div>
+  </div>
+</div>
+
+
+		
+
+<script src="./js/main.js"></script>
+</body>
+</html>
